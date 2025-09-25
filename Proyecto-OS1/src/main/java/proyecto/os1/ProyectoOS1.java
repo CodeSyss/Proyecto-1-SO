2624081d1d88e13bba2ad66aa431fa7aded8ce0f
Carlos Hernández <carlos-hernandez@correo.unimet.edu.ve>
2025-09-24 21:04:11 -0400

    first commit

---
+++ Proyecto-OS1/src/main/java/proyecto/os1/ProyectoOS1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto.os1;
+
+/**
+ *
+ * @author cehernandez
+ */
+public class ProyectoOS1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
