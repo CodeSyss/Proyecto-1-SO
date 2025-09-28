9beb6d92f6f86cb9851e2b6c9d9f213d6bd81896
Fab-Fuertes <fabiana.fuertes@correo.unimet.edu.ve>
2025-09-27 20:30:24 -0400

    pcb, node and stack

--- Proyecto-OS1/src/main/java/main/classes/PCB.java
+++ Proyecto-OS1/src/main/java/main/classes/PCB.java
@@ -9,5 +9,129 @@ package main.classes;
  * @author cehernandez
  */
 public class PCB {
+    private String processName;
+    private int processID;
+    private String state;
+    private int totalInstructions;
+    private int remainingInstructions;
+    private String processType;
+    private int cpuBound;
+    private int ioBound;
+    private int programCounter;
+    private int timeInCpu;
+    private int stackPointer;
+
+    public PCB(String processName, int processID, String state, int totalInstructions, int remainingInstructions, String processType, int cpuBound, int ioBound, int programCounter, int timeInCpu, int stackPointer) {
+        this.processName = processName;
+        this.processID = processID;
+        this.state = state;
+        this.totalInstructions = totalInstructions;
+        this.remainingInstructions = remainingInstructions;
+        this.processType = processType;
+        this.cpuBound = cpuBound;
+        this.ioBound = ioBound;
+        this.programCounter = programCounter;
+        this.timeInCpu = timeInCpu;
+        this.stackPointer = stackPointer;
+        
+         if (cpuBound > ioBound) {
+            this.processType = "CPU-Bound";
+        } else if (ioBound > cpuBound) {
+            this.processType = "I/O-Bound";
+        } else {
+            this.processType = "Balanced";
+        }
+    }
+
+    public String getProcessName() {
+        return processName;
+    }
+
+    public void setProcessName(String processName) {
+        this.processName = processName;
+    }
+
+    public int getProcessID() {
+        return processID;
+    }
+
+    public void setProcessID(int processID) {
+        this.processID = processID;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getTotalInstructions() {
+        return totalInstructions;
+    }
+
+    public void setTotalInstructions(int totalInstructions) {
+        this.totalInstructions = totalInstructions;
+    }
+
+    public int getRemainingInstructions() {
+        return remainingInstructions;
+    }
+
+    public void setRemainingInstructions(int remainingInstructions) {
+        this.remainingInstructions = remainingInstructions;
+    }
+
+    public String getProcessType() {
+        return processType;
+    }
+
+    public void setProcessType(String processType) {
+        this.processType = processType;
+    }
+
+    public int getCpuBound() {
+        return cpuBound;
+    }
+
+    public void setCpuBound(int cpuBound) {
+        this.cpuBound = cpuBound;
+    }
+
+    public int getIoBound() {
+        return ioBound;
+    }
+
+    public void setIoBound(int ioBound) {
+        this.ioBound = ioBound;
+    }
+
+    public int getProgramCounter() {
+        return programCounter;
+    }
+
+    public void setProgramCounter(int programCounter) {
+        this.programCounter = programCounter;
+    }
+
+    public int getTimeInCpu() {
+        return timeInCpu;
+    }
+
+    public void setTimeInCpu(int timeInCpu) {
+        this.timeInCpu = timeInCpu;
+    }
+
+    public int getStackPointer() {
+        return stackPointer;
+    }
+
+    public void setStackPointer(int stackPointer) {
+        this.stackPointer = stackPointer;
+    }
+    
+    
+    
     
 }
---
+++ Proyecto-OS1/src/main/java/main/classes/Stack.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.classes;
+
+/**
+ *
+ * @author fabys
+ */
+public class Stack<T> {
+    private Node<T> top;
+    private int size; 
+    
+     public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
+     
+     
+    public void push(T data) {
+        Node<T> newNode = new Node<>(data);
+
+        newNode.next = top;
+
+        top = newNode;
+
+        size++;
+        System.out.println("PUSH: " + data + " agregado. Tamaño: " + size);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Error: Pila Vacía (Stack Underflow).");
+        }
+
+        T data = top.data;
+        top = top.next;
+
+        size--;
+        System.out.println("POP: " + data + " extraído. Tamaño: " + size);
+        return data;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Error: Pila Vacía.");
+        }
+        return top.data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+
+    public int size() {
+        return size;
+    }
+    
+}
