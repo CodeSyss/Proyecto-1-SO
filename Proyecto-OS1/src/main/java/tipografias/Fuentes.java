f56b90b6f2bb42d0997dbff32af3098b2053a80b
Fab-Fuertes <fabiana.fuertes@correo.unimet.edu.ve>
2025-10-04 11:55:54 -0400

    Principal Jframe

---
+++ Proyecto-OS1/src/main/java/tipografias/Fuentes.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipografias;
+import java.awt.Font;
+import java.io.InputStream;
+
+/**
+ *
+ * @author fabys
+ */
+public class Fuentes {
+    private Font font = null;
+    public String Inter = "/Tipografias/Inter_18pt-SemiBold.ttf";;
+
+
+    /* Font.PLAIN = 0 , Font.BOLD = 1 , Font.ITALIC = 2
+ * tamanio = float
+ */
+    public Font fuente( String fontName, int estilo, float tamanio)
+    {
+         try {
+            //Se carga la fuente
+            InputStream is =  getClass().getResourceAsStream(fontName);
+            font = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (Exception ex) {
+            //Si existe un error se carga fuente por defecto ARIAL
+            System.err.println(fontName + " No se cargo la fuente");
+            font = new Font("Arial", Font.PLAIN, 14);            
+        }
+        Font tfont = font.deriveFont(estilo, tamanio);
+        return tfont;
+    }
+
+}
